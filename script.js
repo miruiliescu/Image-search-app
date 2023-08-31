631eb29e42d7627f9111df762d740a07d0a2cb60
miruiliescu <iliescumiruna@yahoo.com>
2023-08-31 09:09:19 +0300

    imported elements from index.html

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const accessKey = "WIYV0WEKkyiSC9YxJjFS4XgEG_x92Oi_1l0tVo8LRBA"
 
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResults = document.querySelector(".search-results")  //query selector with class name "." before 
+const showMore = document.getElementById("show-more-button")
