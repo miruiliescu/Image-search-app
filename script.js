05a25c5c12682d0b4f7754c505741508ac620754
miruiliescu <iliescumiruna@yahoo.com>
2023-08-31 10:26:50 +0300

    fixes and images are shown with the same height

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-const accessKey = "WIYV0WEKkyiSC9YxJjFS4XgEG_x92Oi_1l0tVo8LRBA"
+const accessKey = "WIYV0WEKkyiSC9YxJjFS4XgEG_x92Oi_1l0tVo8LRBA";
 
-const formEl = document.querySelector("form")
-const inputEl = document.getElementById("search-input")
-const searchResults = document.querySelector(".search-results")  //query selector with class name "." before 
-const showMore = document.getElementById("show-more-button")
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");  
+const showMore = document.getElementById("show-more-button");
 
-let inputData = ""
+let inputData = "";
 let page = 1;
 
 async function searchImages() {
     inputData = inputEl.value;
-    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
 
-    const response = await fetch(url)
-    const data = await response.json()
+    const response = await fetch(url);
+    const data = await response.json();
 
-    //convert json into a variable
 
-    const results = data.results   //store results we are getting from data
+    const results = data.results;   
 
     if (page === 1) {
-        searchResults.innerHTML = " "  
+        searchResults.innerHTML = " "; 
     }
 
-    //map to show images/text one by one
+    
 
-    results.map((map) =>{           //push data in template, generate new boxes
-        const imageWrapper = document.createElement("div"); //container to hold images
+    results.map((result) =>{           
+        const imageWrapper = document.createElement("div"); 
         imageWrapper.classList.add("search-result");
         const image = document.createElement("img");
         image.src = result.urls.small;
         image.alt = result.alt_description;
         const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
         imageLink.target = "_blank";
-        imageLink.textContent = result.alt_descriptions;
+        imageLink.textContent = result.alt_description;
 
         imageWrapper.appendChild(image);
         imageWrapper.appendChild(imageLink);
-        imageWrapper.appendChild(imageWrapper);
+        searchResults.appendChild(imageWrapper);
     });  
 
-   page++
+   page++;
 
    if(page > 1) {
     showMore.style.display = "block";
@@ -50,14 +50,14 @@ async function searchImages() {
 
 
 formEl.addEventListener("submit", (event) => {
-    event.preventDefault()
+    event.preventDefault();
     page = 1;
-    searchImages()
+    searchImages();
 
 })
 
 showMore.addEventListener("click", () => {
     
-    searchImages()
+    searchImages();
 
 })
