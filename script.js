a5ed57f47171063d2591a710edf341ab0ae6bb33
miruiliescu <iliescumiruna@yahoo.com>
2023-08-31 09:39:17 +0300

    created function with dynamic url,  fetch and response

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const formEl = document.querySelector("form")
 const inputEl = document.getElementById("search-input")
 const searchResults = document.querySelector(".search-results")  //query selector with class name "." before 
 const showMore = document.getElementById("show-more-button")
+
+let inputData = ""
+let page = 1;
+
+async function searchImages() {
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    
+
+   
+
+}
